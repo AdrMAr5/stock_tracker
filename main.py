73bb2d048bd15332cacb7f03f31097b279207065
AdrMAr5 <amarkowski@student.agh.edu.pl>
2022-03-28 00:50:34 +0200

    start to set up a db

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import requests
 import sqlite3
 import sqlalchemy
+import pandas as pd
 
 
 def data_validation(dic) -> bool:
 	if not dic['success']:
 		return False
+	return True
 
 
 ACCESS_KEY = '62752580be819ba497213623997646e9'
@@ -15,3 +17,12 @@ URL = 'http://data.fixer.io/api/latest?access_key=' + ACCESS_KEY
 r = requests.get(url=URL)
 if data_validation(r.json()):
 	print(r.json())
+	df = pd.DataFrame.from_dict(data=r.json(), orient='index')
+	print(df)
+
+conn = sqlite3.connect('currencies_stock.db')
+c = conn.cursor()
+# c.execute("""CREATE TABLE IF NOT EXISTS currencies_to_eur(
+# 	currency text,
+#
+# )""")
