2a945857353c71c4881ec0405b7f833f101174d8
AdrMAr5 <amarkowski@student.agh.edu.pl>
2022-03-29 00:37:34 +0200

    divided input data into 3 columns created df

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
 import sqlite3
+import json
 import sqlalchemy
 import pandas as pd
+from datetime import datetime
 
 
-def data_validation(dic) -> bool:
+
+def if_data_valid(dic) -> bool:
 	if not dic['success']:
 		return False
 	return True
@@ -15,10 +18,33 @@ ACCESS_KEY = '62752580be819ba497213623997646e9'
 URL = 'http://data.fixer.io/api/latest?access_key=' + ACCESS_KEY
 
 r = requests.get(url=URL)
-if data_validation(r.json()):
-	print(r.json())
-	df = pd.DataFrame.from_dict(data=r.json(), orient='index')
-	print(df)
+data = r.json()
+if not if_data_valid(data):
+	raise 'invalid data'
+rates = data['rates']
+timestamp = data['timestamp']
+date = data['date']
+new_dict = {'symbol': [], 'rate': [], 'timestamp': []}
+time = data['timestamp']
+
+for key, value in rates.items():
+	new_dict['symbol'].append(key)
+	new_dict['rate'].append(value)
+	new_dict['timestamp'].append(datetime.utcfromtimestamp(int(timestamp)))
+
+
+df = pd.DataFrame(new_dict)
+print(df)
+
+	# print(r.text)
+	# names = r.json()
+	# rates = r.json()['rates']
+	# print(rates)
+	# del r.json()['rates']
+	#
+	# data.update(rates)
+	# #df = pd.DataFrame(data)
+	# print(df)
 
 conn = sqlite3.connect('currencies_stock.db')
 c = conn.cursor()
