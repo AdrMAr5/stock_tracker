a10292eec318ce5b201211d43a17e67d6303d822
AdrMAr5 <amarkowski@student.agh.edu.pl>
2022-04-04 20:39:33 +0200

    Prefect tasks system completed

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sys
-
 import requests
 import sqlite3
 import sqlalchemy
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
+from prefect import task, Flow
+from prefect.schedules import IntervalSchedule
 
 
 class InvalidDataException(Exception):
@@ -20,61 +21,87 @@ headers = {
 }
 
 
-def if_data_valid(req) -> bool:
+@task
+def send_request():
+	r = requests.get(url=URL, headers=headers)
+	print(r.text)
+	data_valid(r)
+	return r
 
-	# check if data changed since last request
-	with open('last_request.txt', 'r+') as f:
-		last_etag = f.readline()
-		cur_etag = r.headers['etag']
-		print(last_etag)
-		print(cur_etag)
-		if last_etag.rstrip() == cur_etag:
-			print('ddd')
+
+def data_valid(resp):
+
+	try:
+		# check if data changed since last request
+		with open('last_request.txt', 'a+') as file:
+			file.seek(0)
+			last_etag = file.readline()
+			cur_etag = resp.headers['etag']
+
+			if last_etag.rstrip() == cur_etag:
+				raise InvalidDataException
+
+			file.seek(0)
+			file.truncate()
+			file.write(cur_etag + '\n')
+
+		# check if operation was successful
+		dic = resp.json()
+		if not dic['success']:
 			raise InvalidDataException
 
-		f.truncate()
-		f.write(cur_etag + '\n')
-
-	# check if operation was successful
-	dic = req.json()
-	if not dic['success']:
-		raise InvalidDataException
-	return True
+	except InvalidDataException:
+		print('dane niepoprawne')
+		sys.exit()
 
 
-r = requests.get(url=URL, headers=headers)
 
-try:
-	if_data_valid(r)
-except InvalidDataException:
-	print('dane niepoprawne')
-	sys.exit()
-data = r.json()
-print(data)
+@task()
+def parse_data(r):
 
-rates = data['rates']
-timestamp = data['timestamp']
-date = data['date']
-new_dict = {'symbol': [], 'rate': [], 'timestamp': []}
-time = data['timestamp']
+	data = r.json()
 
-for key, value in rates.items():
-	new_dict['symbol'].append(key)
-	new_dict['rate'].append(value)
-	new_dict['timestamp'].append(datetime.utcfromtimestamp(int(timestamp)))
+	rates = data['rates']
+	timestamp = data['timestamp']
+	date = data['date']
+	new_dict = {'symbol': [], 'rate': [], 'timestamp': []}
+	time = data['timestamp']
+
+	for key, value in rates.items():
+		new_dict['symbol'].append(key)
+		new_dict['rate'].append(value)
+		new_dict['timestamp'].append(datetime.utcfromtimestamp(int(timestamp)))
+
+	return pd.DataFrame(new_dict)
+
+@task
+def append_to_db(df):
+	engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+	conn = sqlite3.connect('currencies_stock.sqlite')
+	c = conn.cursor()
+	c.execute("""CREATE TABLE IF NOT EXISTS currencies_stock(
+		symbol text,
+		rate text,
+		timestamp text
+	)""")
+	df.to_sql('currencies_stock', engine, index=False, if_exists='append')
 
 
-rates_df = pd.DataFrame(new_dict)
-print(rates_df)
-engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+
+def build_flow():
+	with Flow('stock_tracker_etl') as flow:
+		r = send_request()
+		data = parse_data(r)
+		append_to_db(data)
+
+	return flow
 
 
-conn = sqlite3.connect('currencies_stock.sqlite')
-c = conn.cursor()
-c.execute("""CREATE TABLE IF NOT EXISTS currencies_stock(
-	symbol text,
-	rate text,
-	timestamp text
-)""")
-rates_df.to_sql('currencies_stock', engine, index=False, if_exists='append')
+
+
+flow = build_flow()
+flow.run()
+
+
+
 
